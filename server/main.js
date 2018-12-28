5587f2cdb90a4aa903f6b8905aa7af4336d0579a
Tomy <tomyleereyes@gmail.com>
2018-12-28 13:32:46 -0800

    fix seeds

--- server/main.js
+++ server/main.js
@@ -6,33 +6,31 @@ import '../imports/api/teams/publications.js'
 import { Teams } from '../imports/api/teams/teams.js'
 import { Profiles } from '../imports/api/profiles/profiles.js'
 
-Meteor.startup(() => {
-  Meteor.startup(function() {
-    if (Teams.find().count() === 0) {
-      ;[
-        {
-          _id: '1234567',
-          name: 'Red Cross',
-          description:
-            'Founded to protect human life and health, to ensure respect for all human beings, and to prevent and alleviate human suffering',
-          adminId: '123456'
-        }
-      ].forEach(function(team) {
-        Teams.insert(team)
-      })
-    }
+Meteor.startup(function() {
+  const adminId = Random.id()
+  if (Teams.find().count() === 0) {
+    ;[
+      {
+        name: 'Red Cross',
+        description:
+          'Founded to protect human life and health, to ensure respect for all human beings, and to prevent and alleviate human suffering',
+        adminId: adminId
+      }
+    ].forEach(function(team) {
+      Teams.insert(team)
+    })
+  }
 
-    if (Profiles.find().count() === 0) {
-      ;[
-        {
-          _id: '1234568',
-          name: 'Henry Dunant',
-          bio: 'I am a co-founder of the Red Cross',
-          userId: '123456'
-        }
-      ].forEach(function(profile) {
-        Profiles.insert(profile)
-      })
-    }
-  })
+  if (Profiles.find().count() === 0) {
+    ;[
+      {
+        name: 'Henry Dunant',
+        email: 'WeCare@redcross.ca',
+        bio: 'I am a co-founder of the Red Cross',
+        userId: adminId
+      }
+    ].forEach(function(profile) {
+      Profiles.insert(profile)
+    })
+  }
 })
